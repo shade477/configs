de93818c4ae5804db941a2eb8b59bee9cb3c6eb5
shade477 <106380338+shade477@users.noreply.github.com>
2024-09-23 03:32:36 +0530

    Update provision_kali_wsl.sh
    updated with new tools

--- provision_kali_wsl.sh
+++ provision_kali_wsl.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 
 sudo apt update && sudo apt upgrade -y
+sudo apt install git -y
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/konapun/dotfiles.git
 cd /tmp
-curl https://raw.githubusercontent.com/shade477/configs/main/tmux.conf > .tmux.conf
-curl https://raw.githubusercontent.com/shade477/configs/main/.zshrc > .zshrc
 curl https://raw.githubusercontent.com/shade477/configs/main/proxychains4.conf > proxychains4.conf
 mv .{tmux.conf,zshrc} ~/
 mv proxychains4.conf /etc/
@@ -16,7 +17,6 @@ sudo apt install proxychains4 -y
 sudo apt install freerdp-x11 -y
 sudo apt install python3-impacket -y
 sudo apt install python3.12 -y
-sudo apt install git -y
 sudo apt install smbclient -y
 sudo apt install gcc -y
 sudo apt install vsftpd -y
@@ -35,8 +35,19 @@ sudo apt install metasploit-framework -y
 sudo apt install qemu-utils -y
 sudo apt install responder -y
 sudo apt install sublist3r -y
+sudo apt install exploitdb -y
+sudo apt install neovim -y
+sudo apt install luarocks -y
 python -m pip install --upgrade pip
 sudo python3 -m pip install --user uploadserver
+searchsploit -u
+sudo apt install pipx
+pipx ensurepath
+pipx install git+https://github.com/Pennyw0rth/NetExec
+cd dotfiles
+sudo zsh ./install.zsh -t cyberpunk packages zsh tmux
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 #sudo apt install zsh-autosuggestions
 #sudo apt install zsh-syntax-highlighting
 sudo apt install jadx
